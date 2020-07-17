d6c5bf2dcb6678e3a04a8fd746255f9848d27959
Marius-prog <msabaliauskas@gmail.com>
2020-07-17 14:18:07 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/send', methods=['POST'])
+def send(sum=sum):
+    if request.method == 'POST':
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        operation = request.form['operation']
+    if operation == 'add':
+        sum = float(num1) + float(num2)
+        return render_template('index.html', sum=sum)
+    elif operation == 'subtraction':
+        sum = float(num1) - float(num2)
+        return render_template('index.html', sum=sum)
+    elif operation == 'multiply':
+        sum = float(num1) * float(num2)
+        return render_template('index.html', sum=sum)
+    elif operation == 'divide':
+        sum = float(num1) / float(num2)
+        return render_template('index.html', sum=sum)
+    else:
+        return render_template('index.html')
